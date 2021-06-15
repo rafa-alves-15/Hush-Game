c1896b75bfd97b108c2cff6af320c9a7330f5c94
Rafael Alves <rafaelalves.15@outlook.com>
2021-06-15 18:39:10 -0300

    demo-hush2

---
+++ js/botões.js
@@ -0,0 +1,39 @@
+const elementoHistoria = document.getElementById("historia");
+const elementoImagem = document.getElementById("imagem");
+const elementoButton1 = document.getElementById("button1");
+const elementoButton2 = document.getElementById("button2");
+
+let passoAtual = intro.inicio;
+
+function atualizarTela() {
+
+  elementoImagem.setAttribute("src", passoAtual.imagem);
+
+  if (passoAtual.button1) {
+    elementoButton1.textContent = passoAtual.button1.texto;
+  } else {
+    elementoButton1.remove();
+  }
+  if (passoAtual.button2) {
+    elementoButton2.textContent = passoAtual.button2.texto;
+  } else {
+    elementoButton2.remove();
+  }
+
+
+}
+elementoButton1.addEventListener("click", aoClicarNoBotao);
+elementoButton2.addEventListener("click", aoClicarNoBotao);
+
+function aoClicarNoBotao() {
+  const elementoBotao = this;
+  const botaoClicado = elementoBotao.id;
+  const proximoPasso = passoAtual[botaoClicado].passo;
+  passoAtual = intro[proximoPasso];
+  if (!passoAtual) {
+    alert("Passo " + proximoPasso + " não encontrado!");
+  } else {
+    atualizarTela();
+  }
+}
+atualizarTela();
--- js/hush.js
+++ js/hush.js
@@ -1,38 +1,100 @@
-const intro =
-  "Você é um(a) escritoro(a) que vive uma vida isolada desde que perdeu sua audição quando era adolescente, se colando em um mundo de total silêncio.";
+const intro = {
+  inicio: {
+    historia:
+      "Você é um(a) escritoro(a) que vive uma vida isolada desde que perdeu sua audição quando era adolescente, se colando em um mundo de total silêncio. Sua amiga e vizinha Sarah a visita um dia para devolver uma cópia de um de seus livros, e sarah diz que quer treinar mais libras...",
+    imagem: "Imagens/imagens/sarah.jpg",
+    button1: {
+      texto: "Vocês ensina um pouco de libras para Sarah e conversão um pouco",
+      passo: "h1",
+    },
+    button2: {
+      texto:
+        "Você rejeita ajudar Sarah e diz que está muito ocupada",
+      passo: "h1",
+    },
+  },
+  h1: {
+    historia: "Sarah se despede vai embora",
+    imagem: "Imagens/imagens/sarah-indo-embora.png",
+    button1: {
+      texto: "Você se despede de Sarah, em entra dentro de casa.",
+      passo: "h2",
+    },
+    button2: {
+      texto: "você não fala nada, da as costa e entra dentro de casa.",
+      passo: "h2",
+    },
+  },
+  h2: {
+    historia: "Um pouco mais tarde, você vai pra cozinha lavar o restante da louça, Sarah aparece batendo na porta de vidro da cozinha, mas você não percebe, então o assassino aparece e começa a esfaquear Sarah",
+    imagem: "",
+    button1: {
+      texto:
+        "...",
+      passo: "h3",
+    },
+    button2: {
+      texto:
+        "...",
+      passo: "h3",
+    },
+  },
+  h3: {
+    historia: "",
+    imagem: "images/b01.jpg",
+    button1: {
+      texto:
+        "ESCOLHA 1: Geralt olha bem para o Bardo e o pede para se afastar com a delicadeza de um bruxo...",
+      passo: "h4",
+    },
+    button2: {
+      texto: "ESCOLHA 2: Geralt apenas ignora o Bardo e segue seu caminho...",
+      passo: "h5",
+    },
+  },
+  h4: {
+    historia: "",
+    imagem: "images/b2.gif",
+    button1: {
+      texto: "-Geralt: ...Vem cá, deixa eu te dizer uma coisinha...",
+      passo: "h6",
+    },
+    button2: {
+      texto:
+        "-Jaskier: ...Desgraça... para quê que eu fui mexer com o Lobo branco, arg...",
+      passo: "h6",
+    },
+  },
+  h5: {
+    historia: "",
+    imagem: "images/b21.gif",
+    button1: {
+      texto:
+        "-Jaskier cantarolando: Quando um humilde bardo, Embarcou em uma caminhada, Com Geralt de Rívia, Surgiu esta canção, De quando o Lobo Branco lutou, Contra um diabo astuto...",
+      passo: "h6",
+    },
+    button2: {
+      texto:
+        "...Enquanto os chifres do diabo Esmagavam nossa carne, Então o Bruxo gritou, Ele não pode ser derrotado! Um campeão prevaleceu, Derrotou o vilão, Agora sirvam-lhe uma cerveja...",
+      passo: "h6",
+    },
+  },
+ 
+  fim: {
+    historia: "",
+    imagem: "images/fim.jpg",
+    botao1: null,
+    botao2: null,
+  },
+  umaOp: {
+    historia: "",
+    imagem: "images/b2.jpg",
+    botao1: {
+      texto: "continua",
+      passo: "fim",
+    },
+  },
+};
 
-const elementoImagem = document.getElementById("imagem");
-const elementoBotao1 = document.getElementById("botao1");
-const elementoBotao2 = document.getElementById("botao2");
 
-let passoAtual = historia.inicio;
-
-function atualizarTela() {
-  elementoImagem.setAttribute("src", passoAtual.imagem);
-  if (passoAtual.botao1) {
-    elementoBotao1.textContent = passoAtual.botao1.texto;
-  } else {
-    elementoBotao1.remove();
-  }
-  if (passoAtual.botao2) {
-    elementoBotao2.textContent = passoAtual.botao2.texto;
-  } else {
-    elementoBotao2.remove();
-  }
-}
-elementoBotao1.addEventListener("click", aoClicarNoBotao);
-elementoBotao2.addEventListener("click", aoClicarNoBotao);
-
-function aoClicarNoBotao() {
-  const elementoBotao = this;
-  const botaoClicado = elementoBotao.id;
-  const proximoPasso = passoAtual[botaoClicado].passo;
-  passoAtual = historia[proximoPasso];
-  if (!passoAtual) {
-    alert("Passo " + proximoPasso + " não encontrado!");
-  } else {
-    atualizarTela();
-  }
-}
-
-atualizarTela();
+//Você é um(a) escritoro(a) que vive uma vida isolada desde que perdeu sua audição quando era adolescente, se colando em um mundo de total silêncio. Sua amiga e vizinha Sarah a visita um dia para devolver uma cópia de um de seus livros
