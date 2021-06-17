fabe47e9b94ccd043e0526d5f1123987c5bbedb3
Rafael Alves <rafaelalves.15@outlook.com>
2021-06-17 17:24:42 -0300

    Hush-Game

--- js/hush.js
+++ js/hush.js
@@ -1,32 +1,32 @@
 const intro = {
   inicio: {
     historia:
-      "Você é uma escritora que vive uma vida isolada desde que perdeu sua audição quando era adolescente, se colando em um mundo de total silêncio. Sua amiga e vizinha Sarah a visita um dia para devolver uma cópia de um de seus livros, e sarah diz que quer treinar mais libras...",
+      "Você é uma escritora que vive uma vida isolada desde que perdeu sua audição quando era adolescente, se colocando em um mundo de total silêncio. Sua amiga e vizinha Sarah, à visitar em um dia para devolver uma cópia de um de seus livros, e sarah diz que quer treinar um pouco mais sobre libras...",
     imagem: "Imagens/imagens/sarah.jpg",
     button1: {
-      texto: "Vocês ensina um pouco de libras para Sarah e conversar um pouco",
+      texto: "Vocês ensina libras para Sarah.",
       passo: "h1",
     },
     button2: {
-      texto: "Você rejeita ajudar Sarah e diz que está muito ocupada",
+      texto: "Você rejeita ajudar Sarah e diz que está muito ocupada.",
       passo: "h1",
     },
   },
   h1: {
-    historia: "Sarah se despede vai embora",
+    historia: "Sarah se despede, vai embora.",
     imagem: "Imagens/imagens/sarah-indo-embora.png",
     button1: {
-      texto: "Você se despede de Sarah, em entra dentro de casa.",
+      texto: "Você se despede de Sarah, e em entra para dentro de casa.",
       passo: "h2",
     },
     button2: {
-      texto: "Você não fala nada, da as costa e entra dentro de casa.",
+      texto: "Você não fala nada, e da as costa, e voltando para dentro de casa.",
       passo: "h2",
     },
   },
   h2: {
     historia:
-      "Um pouco mais tarde, você vai a cozinha lavar o restante da louça, Sarah aparece batendo na porta de vidro da cozinha. Então o assassino aparece e começa a esfaquear Sarah",
+      "Um pouco mais tarde, você vai a cozinha, lavar o restante da louça, Sarah aparece batendo na porta de vidro da cozinha. Então o assassino aparece e começa a esfaquear Sarah.",
     imagem: "Imagens/imagens/sarah-morre.png",
     button1: {
       texto: "Proxímo",
@@ -39,7 +39,7 @@ const intro = {
   },
   h3: {
     historia:
-      "O assassino percebe que você ignorou Sarah, e começa a bater o corpo dela para ver sua reação, mas você não o perce, e ele nota que tem algo diferente em você.",
+      "O assassino percebe que você ignorou Sarah, e começa a bater o corpo dela para ver sua reação, mas você não o percebe, e ele nota que tem algo diferente em você.",
     imagem: "Imagens/gifs/observando.gif",
     button1: {
       texto: "Proxímo",
@@ -52,41 +52,41 @@ const intro = {
   },
   h4: {
     historia:
-      "Em seguida você vai para o sofá e abre o Macbook para continuar escrevendo seu livro, e o assasino então na casa e vai se aproximando de você, e nesse exato momento sua irmã Max te liga ",
+      "Em seguida você vai para o sofá e abre o Macbook, para continuar escrevendo seu livro, e o assasino entra em sua casa e vai se aproximando de você, e nesse exato momento sua irmã Max te liga.",
     imagem: "Imagens/imagens/trabalhando.png",
     button1: {
-      texto: "Você aceita a ligação de Max",
+      texto: "Você aceita a ligação de Max.",
       passo: "h5",
     },
     button2: {
-      texto: "Você Recusa a liga de Max",
+      texto: "Você Recusa a liga de Max.",
       passo: "h5",
     },
   },
   h5: {
     historia:
-      "Ao decorrer da conversa com Max, o assasino se esconde e furta seu celular, então Max diz que vê algo se movimentando atraz de você",
+      "Ao decorrer da conversa com Max, o assasino se esconde e furta seu celular, então Max diz que vê algo se movimentando atrás de você.",
     imagem: "Imagens/imagens/furtando-celular.png",
     button1: {
       texto: "Você diz que é só gato.",
       passo: "h6",
     },
     button2: {
-      texto: "Você olha para traz para ver o que é.",
+      texto: "Você olha para trás para ver o que é.",
       passo: "h6",
     },
   },
   h6: {
     historia:
-      "Assim que você desliga a chamada de vídeo com Max, o assassino envia varias fotos suas para seu Macbook, andando pela casa, e fotos de agora sentada no sofá, todas enviadas pelo seu celular",
+      "Assim que você desliga a chamada de vídeo com Max, o assassino envia várias fotos suas para seu Macbook, andando pela casa, e fotos de agora sentada no sofá, todas enviadas pelo seu celular.",
     imagem: "Imagens/imagens/ve-o-assasino.jpg",
     button1: {
-      texto: "Você se levanta e vai até proximo a porta de entrada",
+      texto: "Você se levanta, e vai até proximo a porta de entrada.",
       passo: "h7",
     },
     button2: {
       texto:
-        "Você manda mensagem pra saber quem pegou seu celular e tirou fotos suas",
+        "Você manda mensagem pra saber quem pegou seu celular, e tirou fotos suas.",
       passo: "h7",
     },
   },
