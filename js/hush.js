029d4e1bd0e9e219293e8b2e6b6c063ea914cd71
Rafael Alves <rafaelalves.15@outlook.com>
2021-06-17 15:15:59 -0300

    Demo-HushR

--- js/hush.js
+++ js/hush.js
@@ -32,12 +32,20 @@ const intro = {
       texto: "Proxímo",
       passo: "h3",
     },
+    button2: {
+      texto: "Proxímo",
+      passo: "h3",
+    },
   },
   h3: {
     historia:
       "O assassino percebe que você ignorou Sarah, e começa a bater o corpo dela para ver sua reação, mas você não o perce, e ele nota que tem algo diferente em você.",
     imagem: "Imagens/gifs/observando.gif",
     button1: {
+      texto: "Proxímo",
+      passo: "h4",
+    },
+    button2: {
       texto: "Proxímo",
       passo: "h4",
     },
@@ -48,39 +56,194 @@ const intro = {
     imagem: "",
     button1: {
       texto: "Você aceita a ligação de Max",
-      passo: "h6",
+      passo: "h5",
     },
     button2: {
       texto: "Você Recusa a liga de Max",
-      passo: "h6",
+      passo: "h5",
     },
   },
   h5: {
     historia:
-      "Ao decorrer da conversa com Max, o assasino se esconde e furta seu celular  ",
+      "Ao decorrer da conversa com Max, o assasino se esconde e furta seu celular ",
     imagem: "",
     button1: {
       texto: "Você levanta vai até proximo a porta de entrada",
-      passo: "h7",
+      passo: "h6",
     },
     button2: {
       texto:
         "Você manda mensagem pra saber quem é a pessoa pegou seu celular e tirou fotos suas com ele",
+      passo: "h6",
     },
-    passo: "h7",
   },
-  h5: {
-    historia:
-      "E você vê o um cara de mascará com uma besta e com seu celular na mão ",
+  h6: {
+    historia: "E você vê o um cara de mascará com uma besta nas costa",
     imagem: "",
     button1: {
-      texto: "você corre até",
+      texto: "você corre e fecha a porta.",
+      passo: "h7",
+    },
+    button2: {
+      texto: "você vai na cozinha e pega uma faca.",
+      passo: "h7",
+    },
+  },
+  h7: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
       passo: "h8",
     },
     button2: {
       texto: "",
+      passo: "h8",
     },
-    passo: "h8",
+  },
+  h8: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h9",
+    },
+    button2: {
+      texto: "",
+      passo: "h9",
+    },
+  },
+  h9: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h10",
+    },
+    button2: {
+      texto: "",
+      passo: "h10",
+    },
+  },
+  h10: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h11",
+    },
+    button2: {
+      texto: "",
+      passo: "h11",
+    },
+  },
+  h11: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h12",
+    },
+    button2: {
+      texto: "",
+      passo: "h12",
+    },
+  },
+  h12: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h13",
+    },
+    button2: {
+      texto: "",
+      passo: "h13",
+    },
+  },
+  h13: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h14",
+    },
+    button2: {
+      texto: "",
+      passo: "h14",
+    },
+  },
+  h14: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h15",
+    },
+    button2: {
+      texto: "",
+      passo: "h15",
+    },
+  },
+  h15: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h16",
+    },
+    button2: {
+      texto: "",
+      passo: "h16",
+    },
+  },
+  h16: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h17",
+    },
+    button2: {
+      texto: "",
+      passo: "h17",
+    },
+  },
+  h17: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h18",
+    },
+    button2: {
+      texto: "",
+      passo: "h18",
+    },
+  },
+  h18: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h19",
+    },
+    button2: {
+      texto: "",
+      passo: "h19",
+    },
+  },
+  h19: {
+    historia: "",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h20",
+    },
+    button2: {
+      texto: "",
+      passo: "h20",
+    },
   },
 
   fim: {
@@ -98,3 +261,6 @@ const intro = {
     },
   },
 };
+
+
+//Você é um(a) escritoro(a) que vive uma vida isolada desde que perdeu sua audição quando era adolescente, se colando em um mundo de total silêncio. Sua amiga e vizinha Sarah a visita um dia para devolver uma cópia de um de seus livros
