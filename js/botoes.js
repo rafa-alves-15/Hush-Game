bd4d9240349ea3afc44c680d1833a69fdb72a5a7
Rafael Alves <rafaelalves.15@outlook.com>
2021-06-16 21:13:35 -0300

    Demo-Hush4

---
+++ js/botoes.js
@@ -0,0 +1,100 @@
+const elementoButton = document.getElementById("buttons");
+
+function atualizarTela(elementoHistoria, elementoImagem, elementoButton1, elementoButton2, passoAtual) {
+  
+  elementoHistoria.innerText = passoAtual.historia;
+  
+  elementoImagem.setAttribute("src", passoAtual.imagem);
+
+  if (passoAtual.button1) {
+    elementoButton1.innerText = passoAtual.button1.texto;
+  } else {
+    elementoButton1.remove();
+  }
+  if (passoAtual.button2) {
+    elementoButton2.innerText = passoAtual.button2.texto;
+  } else {
+    elementoButton2.remove();
+  }
+}
+
+function aoClicarNoBotao(
+  elementoBotao,
+  elementoHistoria,
+  elementoImagem,
+  elementoButton1,
+  elementoButton2,
+  passoAtual){
+  const botaoClicado = elementoBotao.id;
+  const proximoPasso = passoAtual[botaoClicado].passo;
+  passoAtual = intro[proximoPasso];
+  if (!passoAtual) {
+    alert("Passo " + proximoPasso + " não encontrado!");
+  } else {
+
+    
+    atualizarTela(
+      elementoHistoria,
+      elementoImagem,
+      elementoButton1,
+      elementoButton2,
+      passoAtual
+    );
+  }
+  }
+
+elementoButton.addEventListener("click", () => {
+
+  let start = document.getElementById("start");
+  let passoAtual = intro.inicio;
+
+  start.innerHTML = `<div>
+        <section class="gameblock">
+            <h1 id="historia" class="imgblock"></h1>
+            <img id="imagem" class="imgblock" src= "" alt="Imagens do jogo">
+        </section>
+       <div>
+
+<section>
+ <div class = click-button>
+        <button id="button1" class="buttons-question"></button>   
+        <button id="button2" class="buttons-question"></button>
+      </div>
+</section>`;
+
+  const elementoHistoria = document.getElementById("historia");
+  const elementoImagem = document.getElementById("imagem");
+  const elementoButton1 = document.getElementById("button1");
+  const elementoButton2 = document.getElementById("button2");
+
+  elementoButton1.addEventListener("click", () => {
+    
+    aoClicarNoBotao(
+      elementoButton1,
+      elementoHistoria,
+      elementoImagem,
+      elementoButton2,
+      passoAtual
+    );
+  });
+  
+  elementoButton2.addEventListener("click", () => {
+    
+    aoClicarNoBotao(
+      elementoButton2,
+      elementoHistoria,
+      elementoImagem,
+      elementoButton1,
+      passoAtual
+    );
+  });
+
+
+  atualizarTela(
+    elementoHistoria,
+    elementoImagem,
+    elementoButton1,
+    elementoButton2,
+    passoAtual
+  );
+});
---
+++ js/hush.js
@@ -0,0 +1,102 @@
+const intro = {
+  inicio: {
+    historia:
+      "Você é uma escritora que vive uma vida isolada desde que perdeu sua audição quando era adolescente, se colando em um mundo de total silêncio. Sua amiga e vizinha Sarah a visita um dia para devolver uma cópia de um de seus livros, e sarah diz que quer treinar mais libras...",
+    imagem: "Imagens/imagens/sarah.jpg",
+    button1: {
+      texto: "Vocês ensina um pouco de libras para Sarah e conversar um pouco",
+      passo: "h1",
+    },
+    button2: {
+      texto: "Você rejeita ajudar Sarah e diz que está muito ocupada",
+      passo: "h1",
+    },
+  },
+  h1: {
+    historia: "Sarah se despede vai embora",
+    imagem: "Imagens/imagens/sarah-indo-embora.png",
+    button1: {
+      texto: "Você se despede de Sarah, em entra dentro de casa.",
+      passo: "h2",
+    },
+    button2: {
+      texto: "você não fala nada, da as costa e entra dentro de casa.",
+      passo: "h2",
+    },
+  },
+  h2: {
+    historia:
+      "Um pouco mais tarde, você vai a cozinha lavar o restante da louça, Sarah aparece batendo na porta de vidro da cozinha. Então o assassino aparece e começa a esfaquear Sarah",
+    imagem: "Imagens/imagens/sarah-morre.png",
+    button1: {
+      texto: "Proxímo",
+      passo: "h3",
+    },
+  },
+  h3: {
+    historia:
+      "O assassino percebe que você ignorou Sarah, e começa a bater o corpo dela para ver sua reação, mas você não o perce, e ele nota que tem algo diferente em você.",
+    imagem: "Imagens/gifs/observando.gif",
+    button1: {
+      texto: "Proxímo",
+      passo: "h4",
+    },
+  },
+  h4: {
+    historia:
+      "Em seguida você vai para o sofá e abre o Macbook para continuar escrevendo seu livro, e o assasino então na casa e vai se aproximando de você, e no exato momento sua irmã Max te liga ",
+    imagem: "",
+    button1: {
+      texto: "Você aceita a ligação de Max",
+      passo: "h6",
+    },
+    button2: {
+      texto: "Você Recusa a liga de Max",
+      passo: "h6",
+    },
+  },
+  h5: {
+    historia:
+      "Ao decorrer da conversa com Max, o assasino se esconde e furta seu celular  ",
+    imagem: "",
+    button1: {
+      texto: "Você levanta vai até proximo a porta de entrada",
+      passo: "h7",
+    },
+    button2: {
+      texto: "Você manda mensagem pra saber quem é a pessoa pegou seu celular e tirou fotos suas com ele",
+    },
+    passo: "h7",
+  },
+  h5: {
+    historia:
+      "E você vê o um cara de mascará com uma besta ",
+    imagem: "",
+    button1: {
+      texto: "",
+      passo: "h8",
+    },
+    button2: {
+      texto: "",
+    },
+    passo: "h8",
+  },
+
+  fim: {
+    historia: "",
+    imagem: "",
+    botao1: null,
+    botao2: null,
+  },
+  umaOp: {
+    historia: "",
+    imagem: "",
+    botao1: {
+      texto: "continua",
+      passo: "fim",
+    },
+  },
+};
+
+
+//Você é um(a) escritoro(a) que vive uma vida isolada desde que perdeu sua audição quando era adolescente, se colando em um mundo de total silêncio. Sua amiga e vizinha Sarah a visita um dia para devolver uma cópia de um de seus livros
